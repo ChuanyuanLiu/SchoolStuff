4b0b17465de64e1daa3eda1e235b067804cf18b5
ChuanyuanLiu <liuchuanyuan@gmail.com>
2018-04-10 23:52:36 +1000

    Added Negative Binomial files

---
+++ MAST20004/NB.py
@@ -0,0 +1,53 @@
+from math import factorial
+import matplotlib.pyplot as plt
+import numpy as np
+
+p = 1/4.0
+r = 2.0
+# to N th term do you want to end?
+N = 21
+
+pmf = []
+
+# choose function
+# returns a float
+
+
+def choose(n, k):
+    return factorial(n)/factorial(k)/factorial(n-k)
+
+# use matplotlib.pyplot to print out a bar graph
+
+
+def draw(pmf):
+    plt.bar(np.arange(N), pmf)
+    plt.xlabel('x')
+    plt.ylabel('p(x)')
+    plt.title('Negative Binomial distribution NB({},{})'.format(r, p))
+    plt.show()
+
+
+def printMD(pmf):
+    odd = 0.0
+    even = 0.0
+    print("z  | p(Z=z)")
+    print("---|---")
+
+    for i, j in enumerate(pmf):
+        print('{:<2} | {:3.4f}'.format(i, j))
+        if i % 2:
+            odd += j
+        else:
+            even += j
+
+    print('total sum of odd possibilties from 0 to {}:{:3.4f}'.format(N, odd))
+    print('total sum of even possibilties from 0 to {}:{:3.4f}'.format(N, even))
+
+
+for z in range(N):
+    pmf.append(choose(z+r-1, r-1)*p**r*(1-p)**z)
+
+# print markdown
+printMD(pmf)
+# draw a bar graph
+draw(pmf)
---
+++ MAST20004/NegativeBinomialDistribution.md
@@ -0,0 +1,36 @@
+# Negative Binomial Distribution
+
+NB(r,p)
+let Z be the number of failure untill rth success
+
+[pythonfile](NB.py)
+
+## What it looks like
+![image](NB.png)
+
+## First 21 z and p(z)
+z  | p(Z=z)
+---|---
+0  | 0.0625
+1  | 0.0938
+2  | 0.1055
+3  | 0.1055
+4  | 0.0989
+5  | 0.0890
+6  | 0.0779
+7  | 0.0667
+8  | 0.0563
+9  | 0.0469
+10 | 0.0387
+11 | 0.0317
+12 | 0.0257
+13 | 0.0208
+14 | 0.0167
+15 | 0.0134
+16 | 0.0106
+17 | 0.0085
+18 | 0.0067
+19 | 0.0053
+20 | 0.0042
+total sum of odd possibilties from 0 to 21:0.4814
+total sum of even possibilties from 0 to 21:0.5037
